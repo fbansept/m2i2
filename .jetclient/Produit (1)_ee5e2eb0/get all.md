51c1341461bdd3e56b1e85e7e0a42eec01d0817f
fbansept <bansept.franck@gmail.com>
2025-07-16 14:43:59 +0200

    move jwt secret into .env

--- .jetclient/Produit (1)_ee5e2eb0/get all.md
+++ .jetclient/Produit (1)_ee5e2eb0/get all.md
@@ -6,5 +6,5 @@ sortWeight = 1000000
 id = '9c95b221-af8f-4c5b-bea6-379217bf51d2'
 
 [auth.bearer]
-token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjQGMifQ.5K0M4QBqtw_-1op7ri5Iii5m4_VeokJm8BSbIYLsIKpEav0uzwGdYzeQJYyAuh9leRMi78BcuySoCsBbeU1G8g'
+token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjQGMifQ.G3QFIAPYmvygCQRNN5WYgVbn72lDPFHC-tfm03z1E4b3oeAF1jPPRT7W2L822-yL0NyIkvG2WIKSngJsIXqdQg'
 ```
--- src/main/java/edu/fbansept/m2i2/security/JwtUtils.java
+++ src/main/java/edu/fbansept/m2i2/security/JwtUtils.java
@@ -2,24 +2,28 @@ package edu.fbansept.m2i2.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUtils {
 
+    @Value("${jwt.secret}")
+    String jwtSecret;
+
     public String generateToken(AppUserDetails userDetails) {
 
        return Jwts.builder()
                 .setSubject(userDetails.getUsername())
-                .signWith(SignatureAlgorithm.HS512, "azerty")
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
 
     }
-    
+
     public String extractSubject(String jwt) {
-        
+
         return Jwts.parser()
-                .setSigningKey("azerty")
+                .setSigningKey(jwtSecret)
                 .parseClaimsJws(jwt)
                 .getBody()
                 .getSubject();
